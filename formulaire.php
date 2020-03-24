d484a61cf9be3a9696b81f32220a7e3777830596
Hugo <hmerle14@gmail.com>
2020-03-24 17:43:17 +0100

    Correct bugs

--- formulaire.php
+++ formulaire.php
@@ -14,19 +14,3 @@ class formulaire{
         echo "</form>";
     }
 }
-
-
-//class Formulaire{
-//    function __construct($nom, $methode){
-//        echo "<form action='".$nom."' method='".$methode."'>";
-//    }
-//    public function addTextZone($text = ""){
-//        echo "$text"."<input name='".$text."' type='text'/><br>";
-//    }
-//    public function addButton($text = ""){
-//        echo"<input type='submit' value='".$text."'/>";
-//    }
-//    public function getForm(){
-//        echo "</form>";
-//    }
-//}
--- testformulaire.php
+++ testformulaire.php
@@ -11,16 +11,3 @@ if(isset($_POST["Nom"]) && isset($_POST["Code"])) {
     echo $_POST['Nom']."<br>";
     echo $_POST['Code'];
 }
-
-//include 'formulaire.php';
-//
-//$form1 = new Formulaire("testformulaire.php", "post");
-//$form1->addTextZone("name");
-//$form1->addTextZone("age");
-//$form1->addButton("Envoi");
-//$form1->getForm();
-//
-//if(isset($_POST["name"]) && isset($_POST["age"])){
-//    echo $_POST["name"]."<br>";
-//    echo $_POST["age"];
-//}
