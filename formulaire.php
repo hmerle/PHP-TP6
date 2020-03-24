fd4e94adec64e689f94cb2a98e1503b167526ce5
Hugo <hmerle14@gmail.com>
2020-03-24 17:01:00 +0100

    Add Class formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -4,9 +4,6 @@
 <?php
 
 class formulaire{
-    public function __construct($methode, $fichierCible){
-        echo "<form method= $methode action='$fichierCible'>";
-    }
     function ajouterZoneDeTexte($text){
         echo $text."<input type='text' name=$text/>";
     }
@@ -15,6 +12,9 @@ class formulaire{
     }
     function getForm(){
         echo "</form>";
+    }
+    public function __construct($methode, $fichierCible){
+        echo "<form method= $methode action='$fichierCible'>";
     }
 }
 
--- testformulaire.php
+++ testformulaire.php
@@ -2,14 +2,16 @@
 include('formulaire.php');
 
 $form = new formulaire("post", "testformulaire.php");
-$form->ajouterZoneDeTexte('Votre nom');
+$form->ajouterZoneDeTexte("Votre nom");
 echo "<br>";
-$form->ajouterZoneDeTexte('Votre code');
+$form->ajouterZoneDeTexte("Votre code");
 echo "<br>";
 $form->ajouterBouton();
 echo "<br>";
 $form->getForm();
 echo "<br>";
 
-echo "Votre nom : ".$_POST['Votre nom']."<br>";
-echo "Votre code : ".$_POST['Votre code']."<br>";
+if(isset($_POST['Votre_nom']) && isset($_POST['Votre_code'])) {
+    echo "Votre nom : " . $_POST['Votre_nom'] . "<br>";
+    echo "Votre code : " . $_POST['Votre_code'] . "<br>";
+}
