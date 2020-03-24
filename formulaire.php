044fe1e2430d481acc67bf88cd52d3c9ce42ac56
Hugo <hmerle14@gmail.com>
2020-03-24 16:18:51 +0100

    Add Class formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,14 +1,24 @@
+<DOCTYPE html>
+    <html lang="fr">
+    <body>
 <?php
-
 
 class formulaire{
     public function __construct($methode, $fichierCible){
         echo "<form method='".$methode."' action='".$fichierCible."'>";
     }
     function ajouterZoneDeTexte(){
-        echo"<input type='text'/>";
+        echo "<input type='text'/>";
     }
     function ajouterBouton(){
-        echo "<input type='submit' value='Envoi'>";
+        echo "<input type='submit' value='Envoi'/>";
+    }
+    function getForm(){
+        echo "</form>";
     }
 }
+
+?>
+    </body>
+    </html>
+</DOCTYPE>
--- testformulaire.php
+++ testformulaire.php
@@ -1,1 +1,6 @@
 <?php
+include formulaire::class;
+
+$form = new formulaire("post", "testformulaire.php");
+$form->ajouterZoneDeTexte();
+$form->ajouterBouton();
